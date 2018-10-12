34e02f7eeac917234b1cf807c5dacea1cf4c18ec
Aseem <aseem.keyal@salesforce.com>
2018-10-11 21:48:12 -0700

    cleaned up data, made findPGs more efficient

--- pg_extraction_utils/findPGs.sh
+++ pg_extraction_utils/findPGs.sh
@@ -2,11 +2,12 @@
 # first argument is packet location, second is opening character of PGs, usually [ or (, second character is second character of PG, usually “? or "? will match it
 # TODO: comment what each step is doing
 
+text="$(pdftotext $1 -)"
 presed="gsed 's/\xe2\x80\x8b\x20//g' | gsed 's/\xe2\x80\x8b//g' | sed 's/\"//g' | sed 's/“//g' | sed 's/”//g' | tr '\n' ' '"
 postsed="sed 's/(/,(/g' | sed \"s/\[/,\[/g\"| sed 's/ ,/,/g'"
 
-pdftotext $1 - | eval $presed | egrep -o "\S+ ?\\$2$3[A-Za-z'’,\-]+$4\\$5" | eval $postsed
-pdftotext $1 - | eval $presed | egrep -o "\S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed
-pdftotext $1 - | eval $presed | egrep -o "\S+ \S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed
-pdftotext $1 - | eval $presed | egrep -o "\S+ \S+ \S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed
-pdftotext $1 - | eval $presed | egrep -o "\S+ \S+ \S+ \S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed 
+echo $text | eval $presed | egrep -o "\S+ ?\\$2$3[A-Za-z'’,\-]+$4\\$5" | eval $postsed
+echo $text | eval $presed | egrep -o "\S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed
+echo $text | eval $presed | egrep -o "\S+ \S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed
+echo $text | eval $presed | egrep -o "\S+ \S+ \S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed
+echo $text | eval $presed | egrep -o "\S+ \S+ \S+ \S+ \S+ ?\\$2$3[A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+ [A-Za-z'’,\-]+$4\\$5" | eval $postsed 
